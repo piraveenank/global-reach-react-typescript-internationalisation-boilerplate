8e8781cc0c51bd9654b509d5cf1287e3185fe297
Piraveenan Kirupakaran <piraveenankirupakaran@yahoo.com>
2023-09-08 07:23:22 +0100

    Fixed Footer

--- src/App.tsx
+++ src/App.tsx
@@ -7,55 +7,55 @@ function App() {
 
   return (
     <>
-      {" "}
-      <ChangeLanguageDropdown></ChangeLanguageDropdown>
-      <div className="app-container">
-        <div className="content-container">
-          <img
-            src="./GlobalReach.png"
-            alt="GlobalReachLogo"
-            className="logo-image"
-          />
-          <h1 className="title" style={{ fontWeight: "800" }}>
-            {formatMessage({ id: "home_title" })}
-          </h1>
-          <h4>{formatMessage({ id: "home_subtitle" })}</h4>
-          <div className="features">
-            <h2 style={{ textDecoration: "underline" }}>
-              {formatMessage({ id: "key_features" })}{" "}
-            </h2>
-            <ul>
-              <li>{formatMessage({ id: "key_features_1" })}</li>
-              <li>{formatMessage({ id: "key_features_2" })}</li>
-              <li>{formatMessage({ id: "key_features_3" })}</li>
-              <li>{formatMessage({ id: "key_features_4" })}</li>
-              <li>{formatMessage({ id: "key_features_5" })}</li>
-              <li>{formatMessage({ id: "key_features_6" })}</li>
-              <li>{formatMessage({ id: "key_features_7" })}</li>
-              <li>{formatMessage({ id: "key_features_8" })}</li>
-            </ul>
+      <div className="app-wrapper">
+        <ChangeLanguageDropdown></ChangeLanguageDropdown>
+        <div className="app-container">
+          <div className="content-container">
+            <img
+              src="./GlobalReach.png"
+              alt="GlobalReachLogo"
+              className="logo-image"
+            />
+            <h1 className="title" style={{ fontWeight: "800" }}>
+              {formatMessage({ id: "home_title" })}
+            </h1>
+            <h4>{formatMessage({ id: "home_subtitle" })}</h4>
+            <div className="features">
+              <h2 style={{ textDecoration: "underline" }}>
+                {formatMessage({ id: "key_features" })}{" "}
+              </h2>
+              <ul>
+                <li>{formatMessage({ id: "key_features_1" })}</li>
+                <li>{formatMessage({ id: "key_features_2" })}</li>
+                <li>{formatMessage({ id: "key_features_3" })}</li>
+                <li>{formatMessage({ id: "key_features_4" })}</li>
+                <li>{formatMessage({ id: "key_features_5" })}</li>
+                <li>{formatMessage({ id: "key_features_6" })}</li>
+                <li>{formatMessage({ id: "key_features_7" })}</li>
+                <li>{formatMessage({ id: "key_features_8" })}</li>
+              </ul>
+            </div>
           </div>
         </div>
+        <footer
+          style={{
+            textAlign: "center",
+            backgroundColor: "black",
+            color: "white",
+            marginTop: "20px",
+            flexShrink: 0,
+          }}>
+          <p>
+            Global Reach - Developed by Piraveenan Kirupakaran
+            <br />
+            <a
+              style={{ color: "white" }}
+              href="https://github.com/piraveenank/global-reach-react-typescript-internationalisation-boilerplate">
+              GitHub
+            </a>
+          </p>
+        </footer>
       </div>
-      <footer
-        id="footer"
-        style={{
-          textAlign: "center",
-          backgroundColor: "black",
-          color: "white",
-          marginTop: "20px",
-          flexShrink: 0,
-        }}>
-        <p>
-          Global Reach - Developed by Piraveenan Kirupakaran
-          <br />
-          <a
-            style={{ color: "white" }}
-            href="https://github.com/piraveenank/global-reach-react-typescript-internationalisation-boilerplate">
-            GitHub
-          </a>
-        </p>
-      </footer>
     </>
   );
 }
